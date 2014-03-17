81846d2ad8222f2a547443f19d25a70943f94030
tang <tangning111@gmail.com>
2014-03-17 20:51:16 +0800

    implemented signup

--- backend.php
+++ backend.php
@@ -28,7 +28,6 @@ if (isset($_REQUEST['action'])) {
 		} else {
 			//find a match in database
 			$dbconn = connectToDatabase($db_name, $db_user, $db_password);
-
 			$select_user_query = ("SELECT * FROM appuser WHERE email = $1 AND password = $2;");
 			$result = pg_prepare($dbconn, "select_user", $select_user_query);
 			$result = pg_execute($dbconn, "select_user", array($email, md5($password)));
@@ -44,8 +43,59 @@ if (isset($_REQUEST['action'])) {
 
 			print json_encode($reply);
 		}
+	} else if($_REQUEST['action'] == 'signup'){
+		$reply = array();
+
+		if (!filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)) {
+			$reply['status'] = 'no';
+			$reply['error'] = 'Please enter a valid email.';
+			print json_encode($reply);
+		} else if (!checkdate($_REQUEST['month'], $_REQUEST['day'], $_REQUEST['year'])) {
+			$reply['status'] = 'no';
+			$reply['error'] = 'Please enter a valid birthday.';
+			print json_encode($reply);
+		} else {
+			//check if this email is already inside database
+			$dbconn = connectToDatabase($db_name, $db_user, $db_password);
+			$select_user_query = ("SELECT * FROM appuser WHERE email = $1;");
+			$result = pg_prepare($dbconn, "select_user", $select_user_query);
+			$result = pg_execute($dbconn, "select_user", array($_REQUEST['email']));
+			
+			if(pg_num_rows($result)){
+				$reply['status'] = 'no';
+				$reply['error'] = 'Email has been registered.';
+				print json_encode($reply);
+			} else {
+				//insert into database
+				$year = $_REQUEST['year'];
+				$month = $_REQUEST['month'];
+				$day = $_REQUEST['day'];
+				$insert_user_query = "INSERT INTO appuser (email, fname, lname, password, birthday, signupdate, news, sex, done) VALUES($1, $2, $3, $4, $5, $6, $7, $8, 0);";
+				$result = pg_prepare($dbconn, "insert_user", $insert_user_query);
+				$result = pg_execute($dbconn, "insert_user", array($_REQUEST['email'], $_REQUEST['fname'], $_REQUEST['lname'], md5($_REQUEST['password']), "$year-$month-$day", date("Y-m-d"), $_REQUEST['news'], $_REQUEST['sex']));
+
+				$reply['status'] = 'ok';
+				print json_encode($reply);
+			}
+		}
+
+
+
+
+
+
 	}
 }
+
+
+
+
+
+
+
+
+
+
 
 function connectToDatabase($db_name, $db_user, $db_password){
         $dbconn = pg_connect("host=localhost port=5432 dbname=$db_name user=$db_user password=$db_password");
@@ -58,5 +108,17 @@ function connectToDatabase($db_name, $db_user, $db_password){
 
         return $dbconn; 
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
--- main.js
+++ main.js
@@ -6,7 +6,14 @@ $(function(){
 	});
 
 	$("#linkToSignUp").on("click", function(){
-		showSignUp();
+		switchLoginAndSignup(1);
+	});
+
+	$("#signupButton").on("click", function(){
+		signup();
+	});
+	$("#gobackButton").on("click", function(){
+		switchLoginAndSignup(0);
 	});
 });
 
@@ -20,10 +27,14 @@ function checkAuthentication(){
 	});
 }
 
-function showSignUp(){
-	$("#login").hide();
-	$("#signup").show();
-	console.log("testing");
+function switchLoginAndSignup(param){
+	if (param == 1) {
+		$("#login").hide();
+		$("#signup").show();
+	} else {
+		$("#login").show();
+		$("#signup").hide();
+	}
 }
 
 function login(){
@@ -41,6 +52,42 @@ function login(){
 			$("#login-form .error").html(data['error']);
 		}
 	});
+}
+
+function signup(){
+	var fname 	= $("#signup-form input[name=fname]").val();
+	var lname 	= $("#signup-form input[name=lname]").val();
+	var email 	= $("#signup-form input[name=email]").val();
+	var password 	= $("#signup-form input[name=password]").val();
+	var repassword 	= $("#signup-form input[name=re-password]").val();
+	var month 	= $("#signup-form select[name=month]").val();
+	var day 	= $("#signup-form select[name=day]").val();
+	var year 	= $("#signup-form select[name=year]").val();
+	var sex 	= $("#signup-form input[name=sex]").val();
+	var news 	= $("#signup-form input[name=news]").val();
+	var policy 	= $("#signup-form input[name=policy]").val();
+
+	//user must fill in all fields
+	if (!fname || !lname || !email || !password || !repassword || month==0 || day==0 || year==0
+		|| !news || !policy) {
+		$("#signup-form .error").show();
+		$("#signup-form .error").html('Please fill in all required fields.');
+	} else if (password != repassword){
+		//passwords do not match
+		$("#signup-form .error").show();
+		$("#signup-form .error").html('Passwords do not match.');
+	} else {
+		//send data to backend.php to further validate and update database
+		$.get('backend.php', {action: "signup", fname: fname, lname: lname, email: email, password:password, month: month, day: day, year: year, sex: sex, news: news, policy: policy}, function(data){
+				console.log(data['status']);
+				if (data['status'] == 'ok') {
+					switchLoginAndSignup(0);
+				} else {
+					$("#signup-form .error").show();
+					$("#signup-form .error").html(data['error']);
+				}
+		});
+	}
 }
 
 
