0c70c86bef5e20fcefb9144959b69045b55d13a4
tang <tangning111@gmail.com>
2014-03-24 13:46:18 +0800

    add function skeleton to model.php

--- model.php
+++ model.php
@@ -1,23 +1,51 @@
 <?php
 //user model---------------------------------------------------
+function findUser(){
 
+}
 
+function addUser(){
 
+}
 
+function updateUser(){
 
+}
+
+function updatePassword(){
+	
+}
 
 
 
 //task model---------------------------------------------------
+function getTasks(){
 
+}
 
+function addTask(){
 
+}
 
+function deleteTask(){
 
+}
 
+function updateTask(){
 
+}
 
+function doTask(){
 
+}
+
+function undoTask(){
+
+}
+
+function doneTask(){
+
+}
 
 
 
