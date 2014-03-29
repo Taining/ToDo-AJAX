c62286f0d5131ff3cf831234484a71da70b4b541
tang <tangning111@gmail.com>
2014-03-29 22:44:04 +0800

    update shcema.sql

--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ CREATE TABLE appuser (
 	fname VARCHAR(20),
 	lname VARCHAR(20),
 	sex INTEGER, -- 1 is female, 2 is male
-	password VARCHAR(100),
+	password VARCHAR(300),
+	salt VARCHAR(10),
 	birthday DATE,
 	news boolean,
 	signupdate DATE,
@@ -31,4 +32,4 @@ CREATE TABLE tasks(
 	
 -- Adding a sample row to appuser TABLE
 -- Password is 12345
-INSERT INTO appuser (email, fname, lname, sex, password, birthday, news, signupdate, done) VALUES ('test@localhost', 'test', 'todo', '2', '827ccb0eea8a706c4c34a16891f84e7b', '2014-02-12', true, '2014-02-12', 0);
+--INSERT INTO appuser (email, fname, lname, sex, password, birthday, news, signupdate, done) VALUES ('test@localhost', 'test', 'todo', '2', '827ccb0eea8a706c4c34a16891f84e7b', '2014-02-12', true, '2014-02-12', 0);
